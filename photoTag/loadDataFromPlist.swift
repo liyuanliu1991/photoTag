d910945d2e4f1067a7f0a2bc0945bfaa4ee6f6b5
Liyuan Liu <515830817@qq.com>
2016-07-04 16:00:05 -0700

    add initalized code

---
+++ photoTag/loadDataFromPlist.swift
@@ -0,0 +1,9 @@
+//
+//  loadDataFromPlist.swift
+//  photoTag
+//
+//  Created by Liyuan Liu on 7/4/16.
+//  Copyright © 2016 Jerry. All rights reserved.
+//
+
+import Foundation
---
+++ photoTag/photoTag/addNewAlbumTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  addNewAlbumTableViewCell.swift
+//  photoTag
+//
+//  Created by Liyuan Liu on 7/4/16.
+//  Copyright © 2016 Jerry. All rights reserved.
+//
+
+import UIKit
+
+class addNewAlbumTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
